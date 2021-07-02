04366df3a3fa3d5b8212ad7bdb35cd1b91840f12
hyeri <hyerijung0903@gmail.com>
2021-07-02 18:06:51 +0900

    Update JpaMemberRepository.java

--- hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class JpaMemberRepository implements MemberRepository {
-
+    //JPA를 쓰려면 EntityManager를 주입받아야한다.
     private  final EntityManager em;
 
     public JpaMemberRepository(EntityManager em) {
@@ -26,6 +26,10 @@ public class JpaMemberRepository implements MemberRepository {
         return Optional.ofNullable(member);
     }
 
+    /*
+    조회/저장/삭제 등은 쿼리를 사용하지 않아도되지만
+    여러개의 리스트, pk가 아닌 것들을 처리하려면 jpql?을 사용해야한다.
+    */
     @Override
     public Optional<Member> findByName(String name) {
         List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
